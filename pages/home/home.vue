43d4bd82b46668d18dd3f8df1d3de0e1c5da4fbe
qmc <1691370112@qq.com>
2023-03-21 11:28:01 +0800

    轮播图创建

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,6 +1,13 @@
 <template>
 	<view>
-	home	
+	<!-- 轮播图的区域	 -->
+  <swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" :circular="true">
+    <swiper-item v-for="(item,i) in swiperList" :key="i">
+      <view class="swiper-item">
+        <image :src="item.image_src"></image>
+      </view>
+    </swiper-item>
+  </swiper>
 	</view>
 </template>
 
@@ -8,12 +15,40 @@
 	export default {
 		data() {
 			return {
-				
+				//这是轮播图的数据列表
+        swiperList: []
 			};
-		}
+		},
+    onLoad(){
+      //调用方法，获取轮播图的数据
+      this.getSwiperList()
+    },
+    methods:{
+      async getSwiperList(){
+        const {data : res} =await uni.$http.get('/api/public/v1/home/swiperdata')
+        //请求失败
+        if(res.meta.status !== 200){
+          return uni.showToast({
+            title:'数据请求失败！',
+            duration:1500,
+            icon:'none'
+          })
+        }
+        //请求成功，为 data 中的数据赋值
+        this.swiperList=res.message
+      }
+    }
 	}
 </script>
 
 <style lang="scss">
-
+  swiper{
+    height: 330rpx;
+    
+    .swiper-item,
+    image{
+      width: 100%;
+      height: 100%;
+    }
+  }
 </style>
