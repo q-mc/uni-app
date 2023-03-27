7d8bdd62aaeceea640f3c5687d3831eba53ed663
qmc <1691370112@qq.com>
2023-03-27 10:39:12 +0800

    完成首页功能开发

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -10,9 +10,32 @@
   </swiper>
   
   <!-- 分类导航区域 -->
-  <view class="nav_list">
-    <view class="nav_item" v-for="(item,i) in navList" :key="i" @click="navClickHandler(item)">
+  <view class="nav-list">
+    <view class="nav-item" v-for="(item,i) in navList" :key="i" @click="navClickHandler(item)">
       <image :src="item.image_src" class="nav-img"></image>
+    </view>
+  </view>
+  
+  <!-- 楼层区域 -->
+  <!-- 楼层的容器 -->
+  <view class="floor-list">
+    <!-- 楼层的item项 -->
+    <view class="floor-item" v-for="(item,i) in floorList" :key="i">
+      <!-- 楼层的标题 -->
+      <image :src="item.floor_title.image_src" class="floor-title"></image>
+      <!-- 楼层的图片区域 -->
+      <view class="floor-img-box">
+        <!-- 左侧大图片的盒子 -->
+        <navigator class="left-img-box" :url="item.product_list[0].url">
+          <image :src="item.product_list[0].image_src" :style="{width : item.product_list[0].image_width + 'rpx'}" mode="widthFix"></image>
+        </navigator>
+        <!-- 右侧4个小图片的盒子 -->
+        <view class="right-img-box">
+          <navigator class="right-img-item" v-for="(item2, i2) in item.product_list" :key="i2" v-if="i2 !== 0" :url="item2.url">
+            <image :src="item2.image_src" mode="widthFix" :style="{width : item2.image_width + 'rpx'}"></image>
+          </navigator>
+        </view>
+      </view>
     </view>
   </view>
 	</view>
@@ -25,7 +48,9 @@
 				//这是轮播图的数据列表
         swiperList: [],
         //分类导航的数据列表
-        navList: []
+        navList: [],
+        //楼层的数据
+        floorList: []
 			};
 		},
     onLoad(){
@@ -33,15 +58,20 @@
       this.getSwiperList()
       //调用方法，获取导航的数据
       this.getNavList()
+      //调用方法，获取楼层的数据
+      this.getFloorList()
     },
     methods:{
+      //获取首页轮播图数据的方法
       async getSwiperList(){
-        const {data : res} =await uni.$http.get('/api/public/v1/home/swiperdata')
+        const {data : res} = await uni.$http.get('/api/public/v1/home/swiperdata')
         //请求失败
         if(res.meta.status !== 200) return uni.$showMsg()
         //请求成功，为 data 中的数据赋值
         this.swiperList=res.message
       },
+      
+      //获取首页导航数据的方法
       async getNavList(){
       const {data : res} = await uni.$http.get('/api/public/v1/home/catitems')
       //请求失败
@@ -49,13 +79,29 @@
       //请求成功，为 data 中的数据赋值
       this.navList=res.message
       },
-      //点击事件
+      //导航点击事件
       navClickHandler(item){
         if(item.name === '分类'){
           uni.switchTab({
             url:'/pages/cate/cate'
           })
         }
+      },
+      
+      //获取首页楼层数据的方法
+      async getFloorList(){
+        const {data : res} = await uni.$http.get('/api/public/v1/home/floordata')
+        //请求失败
+        if(res.meta.status !== 200) return uni.$showMsg()
+        
+        //对数据进行处理
+        res.message.forEach(floor => {
+          floor.product_list.forEach(prod => {
+            prod.url = '/subpkg/goods_list/goods_list?' + prod.navigator_url.split('?')[1]
+          })
+        })
+        //请求成功，为 data 中的数据赋值
+        this.floorList=res.message
       }
     }
 	}
@@ -72,7 +118,7 @@
     }
   }
   
-  .nav_list{
+  .nav-list{
     display: flex;
     justify-content: space-around;
     margin: 15px 0;
@@ -81,5 +127,21 @@
       width: 128rpx;
       height: 140rpx;
     }
+  }
+  
+  .floor-title{
+    width: 100%;
+    height: 60rpx;
+  }
+  
+  .right-img-box{
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+  }
+  
+  .floor-img-box{
+    display: flex;
+    padding-left: 10rpx;
   }
 </style>
